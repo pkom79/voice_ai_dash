e827f12e2e21cd185774bb07fdc71a11b73f7015
pkom79 <pkom79@gmail.com>
2025-11-13 23:24:33 -0500

    Updated UserDetailsPage.tsx

--- src/pages/UserDetailsPage.tsx
+++ src/pages/UserDetailsPage.tsx
@@ -1939,17 +1939,15 @@ export function UserDetailsPage() {
 
             {/* Date Range Picker Modal */}
             {showDatePicker && (
-              <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50 p-4">
-                <div className="bg-white rounded-lg shadow-xl max-w-md w-full">
-                  <DateRangePicker
-                    startDate={startDate}
-                    endDate={endDate}
-                    onStartDateChange={setStartDate}
-                    onEndDateChange={setEndDate}
-                    onClose={() => setShowDatePicker(false)}
-                  />
-                </div>
-              </div>
+              <DateRangePicker
+                startDate={startDate}
+                endDate={endDate}
+                onDateRangeChange={(start, end) => {
+                  setStartDate(start);
+                  setEndDate(end);
+                }}
+                onClose={() => setShowDatePicker(false)}
+              />
             )}
 
             {/* Stats Cards */}
